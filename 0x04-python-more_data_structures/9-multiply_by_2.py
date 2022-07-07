3c705fa4c939666c9361f342dc0be7d162942281
crownpoz <agbeyoadebimpe@gmail.com>
2022-07-07 15:41:48 +0000

    9th task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-	return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+	new_dict = dict(a_dictionary)
+	for key in a_dictionary:
+		new_dict[key] = new_dict[key] * 2
+	return new_dict
