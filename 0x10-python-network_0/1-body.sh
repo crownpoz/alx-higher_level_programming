52352adaa4e5c20d50bd7f7c39b9875bd4631f23
crownpoz <agbeyoadebimpe@gmail.com>
2022-10-20 22:02:59 +0100

    1st task

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"i#!/bin/bash
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"i
